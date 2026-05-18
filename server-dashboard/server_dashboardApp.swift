4d848051f183a901742d2fc17eb1fa9d87071176
Coolio <lmjma13@gmail.com>
2026-05-17 19:07:45 -0700

    Initial Commit

---
+++ server-dashboard/server_dashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  server_dashboardApp.swift
+//  server-dashboard
+//
+//  Created by Larus Mikael Johannsson on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct server_dashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ server-dashboardTests/server_dashboardTests.swift
@@ -0,0 +1,19 @@
+//
+//  server_dashboardTests.swift
+//  server-dashboardTests
+//
+//  Created by Larus Mikael Johannsson on 5/17/26.
+//
+
+import Testing
+@testable import server_dashboard
+
+struct server_dashboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
